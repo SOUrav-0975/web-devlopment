29cb65be873d275126dca3bb901dbfbb7fcb80d1
SOUrav-0975 <souravk0975@gmail.com>
2022-10-12 10:31:08 +0530

    added getelementbytag name assignment

--- script.js
+++ script.js
@@ -25,15 +25,30 @@ headerTitle.style.borderBottom = "solid 2px #000"
 
 
 //GET ELEMENTBYCLASSNAME
-let items=document.getElementsByClassName('list-group-item')
-console.log(items)
+//let items=document.getElementsByClassName('list-group-item')
+//console.log(items)
 
-items[1].textContent="Hello2";
-items[1].style.backgroundColor="yellow";
-items[2].style.backgroundColor= "green"
+//items[1].textContent="Hello2";
+//items[1].style.backgroundColor="yellow";
+//items[2].style.backgroundColor= "green"
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold"
-}
+//for(let i=0;i<items.length;i++){
+  //  items[i].style.fontWeight="bold"
+//}
 
+//GET ELEMENT BY TAG NAME
 
+let li = document.getElementsByTagName("li")
+console.log(li)
+
+console.log(li[1])
+li[1].textContent="Hello";
+li[1].style.fontWeight="bold";
+li[1].style.backgroundColor="red";
+li[4].innerText="This is new list items without any class name"
+//gives error
+//items.style.backgroundColor="#f4f4f4"
+//let items=document.getElementsByClassName('list-group-item')
+//for(let i=0;i<items.length;i++){
+  // items[i].style.backgroundColor="#f4f4f4"
+//}
