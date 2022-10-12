8108c6a1477e3c130815f06371e432f6b9e07ac1
SOUrav-0975 <souravk0975@gmail.com>
2022-10-12 23:25:07 +0530

    added delete and edit part

--- form.js
+++ form.js
@@ -30,7 +30,7 @@ function displayData(){
    let data = JSON.parse(localStorage.getItem("users")) || [];
    let container = document.querySelector("tbody")
    container.innerHTML=null;
-    data.map(function(el){
+    data.map(function(el,index){
         let div = document.createElement("tr")
         let td1 = document.createElement("td");
         td1.innerText = el.name;
@@ -38,8 +38,49 @@ function displayData(){
         td2.innerText=el.email;
         let td3= document.createElement("td");
         td3.innerText=el.phone;
-        div.append(td1,td2,td3)
+        let td4 = document.createElement("td");
+        td4.innerText="Edit"
+        td4.style.color="#fff";
+        td4.style.backgroundColor="yellow";
+        td4.style.cursor="pointer"
+        td4.onclick=() =>{
+           edit (el.name,el.email,el.phone)
+          
+        }
+        let td5 = document.createElement("td");
+        td5.innerText="Delete"
+        td5.style.backgroundColor="red"
+        td5.style.color="#fff";
+        td5.style.cursor="pointer"
+
+       td5.onclick=()=>{
+        remove(index)
+       }
+
+
+        div.append(td1,td2,td3,td4,td5)
         container.append(div)
     })
+}
+displayData();
+//edit
+function edit(name,email,phone){
+    let data = JSON.parse(localStorage.getItem("users")) || []
+    document.getElementById("name").value = name;
+    document.getElementById("email").value = email;
+    document.getElementById("phone").value = phone;
+    localStorage.setItem("users",JSON.stringify(data))
+
+   displayData()
+  }
+//delete
+function remove(id){
+    let data = JSON.parse(localStorage.getItem("users")) || [];
+
+    let newdata = data.filter((el,index)=>{
+        return id !== index;
+    })
+    localStorage.setItem("users",JSON.stringify(newdata))
+    displayData()
 }
 
