90f4975e606b1ed70861de3cca35c2e368ea1b9c
SOUrav-0975 <souravk0975@gmail.com>
2022-10-30 13:06:55 +0530

    added async task

--- Advance JS/Async.js
+++ Advance JS/Async.js
@@ -61,10 +61,54 @@ preMovie().then((m)=>{
 console.log('person4:shows ticket')
 console.log('person5:shows ticket')
 
-async createPost
+//create Post - delete post
+const Posts =[
+    {title:'post-1',body:'this is post-1',createdAT:new Date().getTime()},
+    {title:'post-2',body:'this is post-2',createdAT:new Date().getTime()}
+]
 
+let promise = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        Posts.push({...el,createdAT:new Date().getTime()})
+        const err = false;
+        if(!err){
+            resolve("promise resolve")
+        }else{
+            reject(`Error`)
+        }
+    },4000)
+})
 
+async function createPost(el){
+   try{
+    let result = await promise;
+    console.log(result)
+   }
+   catch(err){
+    console.log(err)
+   }
+    }
+    let deletePromise = new Promise(function(resolve,reject){
+        setTimeout(() => {
+            if(Posts.length > 0){
+                resolve(Posts.pop())
+            }else{
+                reject("Array is Empty")
+            }
+           
+        },1000)
+    })
 
+    function deletePost(){
+        try{
+            let result = await deletePromise;
+            console.log(result)
+           }
+           catch(err){
+            console.log(err)
+           }
+      
+    }
 
 
 
